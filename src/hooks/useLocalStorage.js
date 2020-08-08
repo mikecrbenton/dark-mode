957e378b3e12efae6bde9d1175cfc45605efb12d
mikecrbenton <mikecrbenton@hotmail.com>
2020-08-08 11:02:49 -0500

    project installed

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,10 @@
+import react,{ useState} from 'react';
+
+const useLocalStorage( key, initialValue ){
+
+   //START HERE- WERE GONNA SET UP SOME STATE HERE.... IN README
+   const [] = useState();
+
+}
+
+export default useLocalStorage;
